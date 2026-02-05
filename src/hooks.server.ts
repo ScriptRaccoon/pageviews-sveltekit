df9f838e72a31ace69cbc43e2cff6055821c9608
Script Raccoon <scriptraccoon@gmail.com>
2026-02-05 02:10:37 +0100

    refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,5 @@
-import type { Handle, RequestEvent } from '@sveltejs/kit'
-
-function handle_session(event: RequestEvent) {
-	const saved_session_id = event.cookies.get('session_id')
-	if (saved_session_id) {
-		event.locals.session_id = saved_session_id
-		return
-	}
-
-	const session_id = crypto.randomUUID()
-
-	event.cookies.set('session_id', session_id, {
-		path: '/',
-		maxAge: 60 * 60,
-		sameSite: true,
-		httpOnly: true,
-		secure: true,
-	})
-
-	event.locals.session_id = session_id
-}
+import { handle_session } from '$lib/server/sessions'
+import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
 	handle_session(event)
