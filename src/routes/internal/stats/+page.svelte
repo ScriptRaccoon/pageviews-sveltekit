e17818a4b4a6a6cd5a3be562cca5571d4313662e
Script Raccoon <scriptraccoon@gmail.com>
2026-02-06 09:31:51 +0100

    button to untrack device

--- src/routes/internal/stats/+page.svelte
+++ src/routes/internal/stats/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+	import { browser } from '$app/environment'
+	import { NOTRACK_STORAGE_KEY } from '$lib/client/track'
 	import BarChart from '$lib/components/BarChart.svelte'
 
 	let { data } = $props()
+
+	let device_is_tracked = $state(false)
+
+	if (browser) {
+		device_is_tracked = !window.localStorage.getItem(NOTRACK_STORAGE_KEY)
+	}
+
+	function track_device() {
+		if (!browser) return
+		device_is_tracked = true
+		window.localStorage.removeItem(NOTRACK_STORAGE_KEY)
+	}
+
+	function untrack_devicee() {
+		if (!browser) return
+		device_is_tracked = false
+		window.localStorage.setItem(NOTRACK_STORAGE_KEY, '1')
+	}
 </script>
 
 <header>
@@ -17,6 +37,14 @@
 		<BarChart data={monthly} />
 	</section>
 {/each}
+
+<div class="actions">
+	{#if device_is_tracked}
+		<button class="button" onclick={untrack_devicee}>Do not track this device</button>
+	{:else}
+		<button class="button" onclick={track_device}>Track this device</button>
+	{/if}
+</div>
 
 <style>
 	.path {
@@ -35,5 +63,9 @@
 
 	.total-para {
 		margin-bottom: 2rem;
+	}
+
+	.actions {
+		margin-top: 2rem;
 	}
 </style>
