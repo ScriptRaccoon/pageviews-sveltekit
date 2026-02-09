a9f45b13fdd0e1580cb4e0f7532f6eb6d63b0470
Script Raccoon <scriptraccoon@gmail.com>
2026-02-09 12:11:39 +0100

    redundant code

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,16 +1,7 @@
 import { DB_AUTH_TOKEN, DB_URL } from '$env/static/private'
-import { createClient, type LibsqlError } from '@libsql/client'
+import { createClient } from '@libsql/client'
 
 export const db = createClient({
 	url: DB_URL,
 	authToken: DB_AUTH_TOKEN,
 })
-
-export async function initialize_db() {
-	try {
-		await db.execute('PRAGMA foreign_keys = ON')
-		console.info('Database initialized ✅')
-	} catch (err) {
-		console.error((err as LibsqlError).message)
-	}
-}
